fbd9e1edbf3e3dd83bff33f5df7e608c498f3301
Adam Logan <adamlogan42@gmail.com>
2023-03-22 16:48:05 +0000

    Added Local Database

--- APIs/src/test.ts
+++ APIs/src/test.ts
@@ -3,13 +3,13 @@ import { Pool } from "pg";
 const pool = new Pool({
   host: "postgres",
   user: "postgres",
-  database: "test_database",
-  password: "postgrespw",
+  database: "test_database", 
+  password: "postgrespw", 
   port: 5432
 });
 
 const getUsers = (request: any, response: any) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  pool.query('SELECT * FROM test_table ORDER BY id ASC', (error, results) => {
     if (error) {
       throw error
     }
---
+++ sql/test_database/fill-tables.sql
@@ -0,0 +1,4 @@
+INSERT INTO test_table (name, email)
+    VALUES 
+        ('Jerry', 'jerry@example.com'), 
+        ('George', 'george@example.com');

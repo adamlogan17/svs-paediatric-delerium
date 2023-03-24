6c6f4d5dd03607c13934748b66ac564e02610029
adamlogan17 <adamlogan42@gmail.com>
2023-03-24 11:21:07 +0000

    Fixed format issue of shell script

--- database-set-up.sh
+++ database-set-up.sh
@@ -1,5 +1,3 @@
-#! /bin/bash
-
 echo "Executing Start Up Script"
 
 echo "Creating Roles"
@@ -8,13 +6,13 @@ psql -f "sql/create-roles.sql"
 # the array of the databases to create
 dbs=(test_database audit elearning)
 
-for db in "${dbs[@]}"; do
-    echo "Creating Database ${db}"
+for db in "${dbs[@]}"; do echo "Creating Database ${db}"
     psql -U postgres -c "CREATE DATABASE ${db};"
-
-    for file in sql/${db}/*.sql; do
+    for file in sql/${db}/*.sql
+    do
         # checks if a file exists, and if it does executes it
-        if [ -f "$file" ]; then
+        if [ -f "$file" ]
+        then
             echo "Executing $file"
             psql -d ${db} -f "$file"
         fi

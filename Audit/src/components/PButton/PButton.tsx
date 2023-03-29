6c863852c7b1aac04f6d6b51e7a33edcd698502f
Adam Logan <adamlogan42@gmail.com>
2023-03-29 16:15:54 +0100

    modified PButton component for custom colours

--- Audit/src/components/PButton/PButton.tsx
+++ Audit/src/components/PButton/PButton.tsx
@@ -1,11 +1,28 @@
 import Button from 'react-bootstrap/Button';
-import './PButton.css';
+import { useState } from 'react';
 
-function PButton(props:{text:string, onButtonClick?:any}) {
+function PButton(props:{text:string, onButtonClick?:any, primaryColour?:string, secondaryColour?:string}) {
+  const [isHover, setIsHover] = useState(false);
+
+  function handleMouseEnter() {
+    setIsHover(true);
+  };
+
+  function handleMouseLeave() {
+    setIsHover(false);
+  };
+
+  const primColour:string = props.primaryColour === undefined ? "#009999" : props.primaryColour;
+  const secColour:string = props.secondaryColour === undefined ? "#007e7e" : props.secondaryColour;
+  
+  const newStyle:any = {
+    backgroundColor: isHover ? secColour : primColour,
+    borderColor: primColour
+  };
+
   return (
     <>
-      <Button id = "test" size="lg" onClick = {props.onButtonClick}>{props.text}</Button>{' '}
-
+      <Button style={newStyle} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} size="lg" onClick = {props.onButtonClick}>{props.text}</Button>{' '}
     </>
   );
 }
--- Audit/src/pages/login/login.tsx
+++ Audit/src/pages/login/login.tsx
@@ -1,16 +1,30 @@
 import BasicNavBar from '../../components/NavBar/NavBar';
-import HeadingText from '../../components/HeadingText/HeadingText'
-import BodyText from '../../components/BodyText/BodyText'
-import PButton from '../../components/PButton/PButton'
-import ContactInfo from '../../components/ContactInfo/ContactInfo'
+import PButton from '../../components/PButton/PButton';
+
+import '../../shared/layout.css';
+import './login.css'
 
 function Login() {
   return (
-        <div className='wrapper'>
+        <div id='login' className='wrapper'>
             <BasicNavBar />
             
             <div className = 'content'>
-                <h1>Login</h1>
+                <h1>Please Login</h1>
+                <br />
+
+                <form action="" method="get">
+                    <div className="data-input">
+                        <label htmlFor="username">PICU Site Number:</label>
+                        <input id="username" className="entry" type="text" name="username"/>
+                        <br />
+
+                        <label htmlFor="password">Password:</label>
+                        <input id="password" className="entry" type="text" name="password"/>
+                        <br />
+                    </div>
+                    <PButton text="Start"/>
+                </form>
             </div>
         </div>
   );

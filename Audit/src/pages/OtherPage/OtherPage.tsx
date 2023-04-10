25e88b148f37b256c2d0bc2b015122bac2b2b507
Adam Logan <adamlogan42@gmail.com>
2023-04-10 21:21:09 +0100

    Added test auth API to otherPage

--- Audit/src/pages/OtherPage/OtherPage.tsx
+++ Audit/src/pages/OtherPage/OtherPage.tsx
@@ -1,5 +1,30 @@
+import axios from "axios";
+import PButton from "../../components/PButton/PButton";
+
+function testAuth() {
+    const configuration = {
+        method: "get",
+        url: "http://localhost:8000/test-auth",
+        headers: { 'Authorization': "bearer " + sessionStorage.getItem('TOKEN') } 
+    };
+    
+    // make the API call
+    axios(configuration)
+        .then((result) => {
+            // sets the cookies
+            console.log(result.data.message);
+        })
+        .catch((error) => error = new Error());
+}
+
 const OtherPage = () => {
-    return <h1>Page</h1>;
+    return (
+        <>
+            <h1>Page</h1>
+            
+            <PButton text="Test Auth" primaryColour='#025858' secondaryColour='#013e3e' onButtonClick={()=>testAuth()} />
+        </>
+    );
 };
 
 export default OtherPage;

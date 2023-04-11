401c45c95b9481d70278675d803115bdb4e07129
Andrew Robb <arobb09@qub.ac.uk>
2023-04-11 20:43:44 +0100

    AuditGraph page created & boilerplate implemented

---
+++ Audit/src/pages/AuditGraphs/AuditGraphs.tsx
@@ -0,0 +1,29 @@
+import BasicNavBar from '../../components/NavBar/NavBar';
+
+import '../../shared/layout.css';
+
+function AuditGraphs() {
+  return (
+        <div id='home' className='wrapper'>
+          
+          <BasicNavBar />
+          <div className = 'content'>
+            <div className = 'row' id = 'UpperTextContainer'>
+              <div className='col'>
+                
+                </div>
+            </div>
+
+            <div className = 'row' id = 'ButtonContainer'>
+              
+            </div>
+
+            <div className = 'row' id = 'ContactInfoContainer'>
+              
+            </div>
+          </div>
+        </div>
+  );
+}
+
+export default AuditGraphs;

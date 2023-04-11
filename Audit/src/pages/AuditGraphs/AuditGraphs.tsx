18e4237388f230743a11ecb5e2e2ee6404aece98
Andrew Robb <arobb09@qub.ac.uk>
2023-04-11 21:40:03 +0100

    Basic page structure implemented

--- Audit/src/pages/AuditGraphs/AuditGraphs.tsx
+++ Audit/src/pages/AuditGraphs/AuditGraphs.tsx
@@ -1,14 +1,42 @@
 import BasicNavBar from '../../components/NavBar/NavBar';
+import TypeDropDown from '../../components/TypeDropDown/TypeDropDown';
+import PButton from '../../components/PButton/PButton';
 
 import '../../shared/layout.css';
+import BodyText from '../../components/BodyText/BodyText';
 
 function AuditGraphs() {
   return (
-        <div id='home' className='wrapper'>
+        <div id='form' className='wrapper'>
           
           <BasicNavBar />
+
           <div className = 'content'>
-            <div className = 'row' id = 'UpperTextContainer'>
+          <h1>Delirium Compliance - Audit Form</h1>
+            <br />
+
+          <form action="" method="get">
+            <div className="data-input">
+                <div className = 'row' id = 'UpperTextContainer'>
+                        <div className='col'>
+                            <h5>Select your PICU number from this list: </h5>
+                            <TypeDropDown text="Site Number" primaryColour='#025858' secondaryColour='#013e3e' options={Array.from({length: 29}, (_, i) => ("s" + (i + 1)).toString())}/>
+                        </div>
+                        <div className='col'>
+                            <h5>Select the type of chart you would like: </h5>
+                            <TypeDropDown text="Chart Type" primaryColour='#025858' secondaryColour='#013e3e' options={["Line Graph", "Pie Chart", "Bar Chart"]}/>
+                        </div>
+                </div>
+    
+            </div>
+            <div className = 'row' id = 'ButtonContainer'>
+                <BodyText text = 'THIS IS JUST A PLACEHOLDER TEXT BOX FOR WHAT WILL BE THE GRAPH CANVAS'/>
+            </div>
+                <PButton text="Submit" onButtonClick = {() => {console.log("Hello World")}} primaryColour='#025858' secondaryColour='#013e3e'/>
+            </form>
+            
+            
+            {/* <div className = 'row' id = 'UpperTextContainer'>
               <div className='col'>
                 
                 </div>
@@ -20,7 +48,7 @@ function AuditGraphs() {
 
             <div className = 'row' id = 'ContactInfoContainer'>
               
-            </div>
+            </div> */}
           </div>
         </div>
   );

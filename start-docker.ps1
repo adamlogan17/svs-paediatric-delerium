f350f2e8118e47cd4e3884d40f15895e026c6729
adamlogan17 <adamlogan17@users.noreply.github.com>
2023-10-17 11:24:50 +0100

    made dev docker compose

--- start-docker.ps1
+++ start-docker.ps1
@@ -11,6 +11,7 @@ param(
 
 docker-compose down
 
+# TODO delete all the containers just for the project, and the same with the volumes, will need to name the volumes
 if($c -or $n) {
     # removes all containers
     docker rm -f $(docker ps -a -q)

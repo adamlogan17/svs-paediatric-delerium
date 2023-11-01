d61acac194ffb9dba768d46d076e031b20b47cc0
Adam Logan <adamlogan42@gmail.com>
2023-11-01 19:32:26 +0000

    Improved start-docker scripts and auto added server to pgadmin4

--- start-docker.sh
+++ start-docker.sh
@@ -1,66 +1,80 @@
-ARGS=$(getopt -a --options bcn --long "background,clean,nuclear" -- "$@")
+#!/bin/bash
 
-eval set -- "$ARGS"
-
-background="false"
-clean="false"
-nuclear="false"
-production="false"
-
-
-while true; do
+# Parse command-line options
+while [[ $# -gt 0 ]]; do
     case "$1" in
-    -b|--background)
-        background="true"
-        shift;;
-    -c|--clean)
-        clean="true"
-        shift;;
-    -n|--nuclear)
-        nuclear="true"
-        shift;;
-    -n|--production)
-        production="true"
-        shift;;
-    --)
-        break;;
+        -b|--b)
+            b=true
+            shift
+            ;;
+        -c|--c)
+            c=true
+            shift
+            ;;
+        -n|--n)
+            n=true
+            shift
+            ;;
+        -p|--p)
+            p=true
+            shift
+            ;;
         *)
-        printf "Unknown option %s\n" "$1"
-        exit 1;;
+            echo "Unknown option: $1"
+            exit 1
+            ;;
     esac
 done
 
 docker-compose down
 
 if [ "$c" ] || [ "$n" ]; then
-    # removes all containers
-    docker rm -a -f -v $(docker ps -q -f name="svs")
-fi
-
-if [ -z "$p" ]; then
-    # removes the postgres server volume
+    # Remove the postgres server volume
+    echo "Removing all containers with 'svs' in the name..."
+    echo ""
+    docker rm -v -f $(docker ps -q -a -f name="svs")
+    echo ""
+    echo "Remove complete!"
+    echo ""
+elif [ -z "$p" ]; then
+    # Remove the postgres server volume
     docker volume rm svs-paediatric-delerium_svs-data
 fi
 
 # Check if n flag is set
 if [ "$n" ]; then
-    # removes all images related to the project
-    docker rmi $(docker images -a -f ancestor=postgres -q)
-    docker rmi $(docker images -a -f ancestor=svs-paediatric-delerium-audit -q)
-    docker rmi $(docker images -a -f ancestor=svs-paediatric-delerium-apis -q)
+    # removes all images associated with the project
+    echo "Removing the svs images, postgres image, and pgadmin4 image..."
+    echo ""
+    docker rmi $(docker images -a postgres -q)
+    docker rmi $(docker images -a dpage/pgadmin4 -q)
+    docker rmi $(docker images -a svs-paediatric-delerium-audit -q)
+    docker rmi $(docker images -a svs-paediatric-delerium-apis -q)
+    echo ""
+    echo "Remove complete!"
+    echo ""
 fi
 
 # Check if b flag is set
 if [ "$b" ]; then
-    # starts all containers in the background
+    echo "Starting DEV containers in the background..."
+    echo ""
     docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 elif [ "$p" ]; then
+    echo "Removing PROD containers..."
+    echo ""
     docker rm -a -v -f $(docker ps -q -f name="prod_svs")
-
-    docker rmi $(docker images -a -f ancestor=svs-paediatric-delerium-audit -q)
-    docker rmi $(docker images -a -f ancestor=svs-paediatric-delerium-apis -q)
-
+    echo ""
+    echo "Remove complete!"
+    echo ""
+    
+    remove_project_images
+    
+    echo "Starting PROD containers..."
+    echo ""
     docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
 else
+    echo "Starting DEV containers..."
+    echo ""
     docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
 fi

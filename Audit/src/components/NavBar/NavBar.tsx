312d5cbaed065c109b20b6f05fc0d3442fa7ee2e
40287450 <rduncan10@qub.ac.uk>
2023-11-08 17:28:02 +0000

    Prep for Charles Demo

--- Audit/src/components/NavBar/NavBar.tsx
+++ Audit/src/components/NavBar/NavBar.tsx
@@ -1,222 +1,103 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
+import {
+   MDBNavbar,
+   MDBContainer,
+   MDBIcon,
+   MDBNavbarNav,
+   MDBNavbarItem,
+   MDBNavbarLink,
+   MDBNavbarToggler,
+   MDBNavbarBrand,
+   MDBCollapse
+ } from 'mdb-react-ui-kit';
 import PDLongLogo from '../../assets/images/transparent-PD-long-logo.png';
-
+import { Select, MenuItem } from '@mui/material';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import IconButton from '@mui/material/IconButton';
+import { Paper } from '@mui/material';
+import { text } from 'stream/consumers';
 
 /**
  * The NavBar to be used site wide and should be placed at the top of the page
  * @author Adam Logan
  * @date 2023-04-28
  */
-function BasicNavBar({props}:any) {
-  return (
-    // change bit by bit to keep hamburger menu
-      <Navbar bg={props.mode} expand="lg"> 
-      <Container>
-        <Navbar.Brand href="#home">
-            <img src={PDLongLogo}  
-            alt="logo"
-            width="210"
-            height="50"
-            className="d-inline-block align-top" />        
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Background</Nav.Link>
-            <Nav.Link href="#home">Team</Nav.Link>
-            <Nav.Link href="#home">How to use the Audit System</Nav.Link>
-            <Nav.Link href="#home">Contact us</Nav.Link>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+export default function Navbar(props:{theme:string, toggleMode:Function, backgroundColor:string, textColor:string}) {
+     const [showNav, setShowNav] = useState(false);
+     const [showNavColor, setShowNavColor] = useState(false);
+     const options = [
+       { label: "Contrast", value: "contrast" },
+       { label: "Dark", value: "dark" },
+       { label: "Light", value: "light" },
+     ];
 
-            <IconButton sx={{ ml: 1 }} onClick={() => props.toggleMode()} >
-              {props.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-            </IconButton>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
-export default BasicNavBar;
-
-
-
-
-
-
-// import PDLongLogo from '../../assets/images/transparent-PD-long-logo.png';
-// import { useState } from 'react';
-// import {
-//   MDBNavbar,
-//   MDBContainer,
-//   MDBIcon,
-//   MDBNavbarNav,
-//   MDBNavbarItem,
-//   MDBNavbarLink,
-//   MDBNavbarToggler,
-//   MDBNavbarBrand,
-//   MDBCollapse
-// } from 'mdb-react-ui-kit';
-// import { Select, MenuItem } from '@mui/material';
-// import Brightness7Icon from '@mui/icons-material/Brightness7';
-// import Brightness4Icon from '@mui/icons-material/Brightness4';
-// import IconButton from '@mui/material/IconButton';
-// import { Paper } from '@mui/material';
-// import { text } from 'stream/consumers';
-
-
-// //import PageProgress from '../PageProgress/PageProgress';import Container from 'react-bootstrap/Container';
-
-// export default function Navbar(props:{theme:string, toggleMode:Function, backgroundColor:string, textColor:string}) {
-//   const [showNav, setShowNav] = useState(false);
-//   const [showNavColor, setShowNavColor] = useState(false);
-//   const options = [
-//     { label: "Contrast", value: "contrast" },
-//     { label: "Dark", value: "dark" },
-//     { label: "Light", value: "light" },
-//   ];
-  
-//   return (
-//     <div>
-//       <Paper sx={{ borderRadius:'0px' }}>
-//         <MDBNavbar dark expand='lg'>
-          
-//           <MDBContainer >
-//             <MDBNavbarBrand>
-//               <img
-//               src={PDLongLogo}  
-//               alt="logo"
-//               width="210"
-//               height="50"
-//               className="d-inline-block align-top"
-//               />
-//               <Select
-//             sx={{marginLeft:'10px'}}
-//             value={props.theme}
-//             onChange={(e) => props.toggleMode(e.target.value)}
-//           >
-//             {options.map((option, index) => 
-//               <MenuItem key={index} value={option.value}>
-//                 {option.label}
-//               </MenuItem>
-//             )}
-//           </Select>
-//             </MDBNavbarBrand >
+     return (
+           <div>
+             <Paper sx={{ borderRadius:'0px' }}>
+               <MDBNavbar dark expand='lg'>
             
-//             <MDBNavbarToggler  style={{display:'inline', zIndex:100}}
-//             onClick={() => setShowNav(!showNav)}>
-//               <MDBIcon icon= 'bars'  fas />
-//             </MDBNavbarToggler>
-
-//             <MDBCollapse navbar show={showNav} >
-//               <MDBNavbarNav  style={{ justifyContent:'right' }}>
-//                 <MDBNavbarItem>
-//                 <MDBNavbarLink style={{color:'inherit'}} href="#home" onClick={() => setShowNav(!showNav)}>Background</MDBNavbarLink>
-//                 </MDBNavbarItem>
-
-//                 <MDBNavbarItem>
-//                   <MDBNavbarLink style={{color:'inherit'}} href='#home' onClick={() => setShowNav(!showNav)}>Team</MDBNavbarLink>
-//                 </MDBNavbarItem>
-
-//                 <MDBNavbarItem>
-//                   <MDBNavbarLink style={{color:'inherit'}} href='#home' onClick={() => setShowNav(!showNav)}>How to Use</MDBNavbarLink>
-//                 </MDBNavbarItem>
-                
-//                 <MDBNavbarItem>
-//                   <MDBNavbarLink style={{color:'inherit'}} href='#home'  onClick={() => setShowNav(!showNav)}>Contact Us</MDBNavbarLink>
-//                 </MDBNavbarItem>
-
-//                 <MDBNavbarItem>
-//                   <MDBNavbarLink style={{color:'inherit'}} href='/'  onClick={() => setShowNav(!showNav)}>Home</MDBNavbarLink>
-//                 </MDBNavbarItem>
-
-//                 <MDBNavbarItem>
-//                   <MDBNavbarLink style={{color:'inherit'}} href='/login'  onClick={() => setShowNav(!showNav)}>Log in</MDBNavbarLink>
-//                 </MDBNavbarItem>
-//               </MDBNavbarNav>
-//             </MDBCollapse>
-//           </MDBContainer>
-//         </MDBNavbar>
-//       </Paper>
-     
-//     </div>
-    
-//   );
-// }
-
-
-
-
-
-
-/**import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import PDLongLogo from '../../assets/images/transparent-PD-long-logo.png';
-
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import IconButton from '@mui/material/IconButton';
-import { Select, MenuItem } from '@mui/material';
-
-/**
- * The NavBar to be used site wide and should be placed at the top of the page
- * @author Adam Logan
- * @date 2023-04-28
- 
-function BasicNavBar(props:{theme:string, toggleMode:Function, backgroundColor:string, textColor:string}) {
-  const options = [
-    { label: "Contrast", value: "contrast" },
-    { label: "Dark", value: "dark" },
-    { label: "Light", value: "light" },
-  ];
-
-  return (
-    <Navbar expand="lg" style={{ backgroundColor:props.backgroundColor, color:props.textColor}}>
-      <Container>
-        <Navbar.Brand>
-            <img 
-              src={PDLongLogo}  
-              alt="logo"
-              width="210"
-              height="50"
-              className="d-inline-block align-top" 
-            />
-          {/* <IconButton sx={{ ml: 1 }} onClick={() => props.toggleMode('light' ? 'dark' : 'light')} >
-            {props.theme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-          </IconButton> }
-          <Select
-            sx={{marginLeft:'10px'}}
-            value={props.theme}
-            onChange={(e) => props.toggleMode(e.target.value)}
-          >
-            {options.map((option, index) => 
-              <MenuItem key={index} value={option.value}>
-                {option.label}
-              </MenuItem>
-            )}
-          </Select>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" >
-            <Nav.Link href="#home" style={{color:'inherit'}}>Background</Nav.Link>
-            <Nav.Link href="#home" style={{color:'inherit'}}>Team</Nav.Link>
-            <Nav.Link href="#home" style={{color:'inherit'}}>How to use the Audit System</Nav.Link>
-            <Nav.Link href="#home" style={{color:'inherit'}}>Contact us</Nav.Link>
-            <Nav.Link href="/" style={{color:'inherit'}}>Home</Nav.Link>
-            <Nav.Link href="/login" style={{color:'inherit'}}>Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
-export default BasicNavBar;*/
+                 <MDBContainer >
+                   <MDBNavbarBrand>
+                     <img
+                     src={PDLongLogo}  
+                     alt="logo"
+                     width="210"
+                     height="50"
+                     className="d-inline-block align-top"
+                     />
+                     <Select
+                   sx={{marginLeft:'10px'}}
+                   value={props.theme}
+                   onChange={(e) => props.toggleMode(e.target.value)}
+                 >
+                   {options.map((option, index) => 
+                     <MenuItem key={index} value={option.value}>
+                       {option.label}
+                     </MenuItem>
+                   )}
+                 </Select>
+                   </MDBNavbarBrand >
+              
+                   <MDBNavbarToggler  style={{borderColor: props.textColor}}
+                   onClick={() => setShowNav(!showNav)}>
+                     <MDBIcon icon= 'bars'  fas />
+                   </MDBNavbarToggler>
+  
+                   <MDBCollapse navbar show={showNav} >
+                     <MDBNavbarNav  style={{ justifyContent:'right' }}>
+                       <MDBNavbarItem>
+                       <MDBNavbarLink style={{color:'inherit'}} href="#home" onClick={() => setShowNav(!showNav)}>Background</MDBNavbarLink>
+                       </MDBNavbarItem>
+  
+                       <MDBNavbarItem>
+                         <MDBNavbarLink style={{color:'inherit'}} href='#home' onClick={() => setShowNav(!showNav)}>Team</MDBNavbarLink>
+                       </MDBNavbarItem>
+  
+                       <MDBNavbarItem>
+                         <MDBNavbarLink style={{color:'inherit'}} href='#home' onClick={() => setShowNav(!showNav)}>How to Use</MDBNavbarLink>
+                       </MDBNavbarItem>
+                  
+                       <MDBNavbarItem>
+                         <MDBNavbarLink style={{color:'inherit'}} href='#home'  onClick={() => setShowNav(!showNav)}>Contact Us</MDBNavbarLink>
+                       </MDBNavbarItem>
+  
+                       <MDBNavbarItem>
+                         <MDBNavbarLink style={{color:'inherit'}} href='/'  onClick={() => setShowNav(!showNav)}>Home</MDBNavbarLink>
+                       </MDBNavbarItem>
+  
+                       <MDBNavbarItem>
+                         <MDBNavbarLink style={{color:'inherit'}} href='/login'  onClick={() => setShowNav(!showNav)}>Log in</MDBNavbarLink>
+                       </MDBNavbarItem>
+                     </MDBNavbarNav>
+                   </MDBCollapse>
+                 </MDBContainer>
+               </MDBNavbar>
+             </Paper>
+       
+           </div>
+      
+         );
+       }

54ad336dcd175e01494f75c7b5880cdccac97b76
adamlogan17 <adamlogan42@gmail.com>
2023-11-14 14:17:07 +0000

    added signout functionality

--- Audit/src/components/NavBar/NavBar.tsx
+++ Audit/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,9 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import PDLongLogo from '../../assets/images/transparent-PD-long-logo.png';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import IconButton from '@mui/material/IconButton';
 import { Select, MenuItem, Container } from '@mui/material';
-import BorderColor from '@mui/icons-material/BorderColor';
+import { useState } from 'react';
+import ConfirmDialog from '../ConfirmDialog/ConfirmDialog';
 
 /**
  * The NavBar to be used site wide and should be placed at the top of the page
@@ -13,9 +11,19 @@ import BorderColor from '@mui/icons-material/BorderColor';
  * @date 2023-04-28*/
 
 function BasicNavBar(props:{theme:string, toggleMode:Function, backgroundColor:string, textColor:string, buttonColor:string, modeOptions:{label:string, value:string}[]}) {
+  const [showDialog, setShowDialog] = useState<boolean>(false);
+
+  function signOut():void {
+    sessionStorage.removeItem('TOKEN');
+    sessionStorage.removeItem('ROLE');
+    sessionStorage.removeItem('USERNAME');
+    window.location.href="/";
+  }
+
   return (
     <Container>
-    <Navbar expand="lg" style={{ backgroundColor:props.backgroundColor, color:props.textColor}}>
+      <ConfirmDialog open={showDialog} title={'Sign Out'} description={<>Would you like to sign out?</>} handleConfirm={signOut} handleClose={() => null}/>
+      <Navbar expand="lg" style={{ backgroundColor:props.backgroundColor, color:props.textColor}}>
         <Navbar.Brand>
             <img 
               src={PDLongLogo}  
@@ -44,10 +52,17 @@ function BasicNavBar(props:{theme:string, toggleMode:Function, backgroundColor:s
             <Nav.Link href="#home" style={{color:'inherit'}}>How to use the Audit System</Nav.Link>
             <Nav.Link href="#home" style={{color:'inherit'}}>Contact us</Nav.Link>
             <Nav.Link href="/" style={{color:'inherit'}}>Home</Nav.Link>
-            <Nav.Link href="/login" style={{color:'inherit'}}>Login</Nav.Link>
+
+            {
+              (sessionStorage.getItem('TOKEN') ? (
+                <Nav.Link onClick={() => setShowDialog(true)} style={{color:'inherit'}}>Sign Out</Nav.Link>
+              ) : (
+                <Nav.Link href="/login" style={{color:'inherit'}}>Login</Nav.Link>
+              ))
+            }
           </Nav>
         </Navbar.Collapse>
-    </Navbar>
+      </Navbar>
     </Container>
   );
 }

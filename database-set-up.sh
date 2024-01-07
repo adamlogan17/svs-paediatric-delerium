e4466eca094aa241b1d59ac4f4ba4424f596a083
40289650 <eforsythe05@qub.ac.uk>
2024-01-07 13:57:18 +0000

    added debug output

--- database-set-up.sh
+++ database-set-up.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 dbs=(test_database audit elearning backup)
-#hello
+
+echo "Creating databases..."
+
 for db in "${dbs[@]}"; do 
     psql -U postgres -c "CREATE DATABASE ${db};"
     for file in sql/${db}/*.sql

bbe677f5e7c23c386edc60cd9b81b1e7a6c55df7
40289650 <eforsythe05@qub.ac.uk>
2024-01-07 10:57:22 +0000

    removed echo

--- database-set-up.sh
+++ database-set-up.sh
@@ -1,17 +1,17 @@
-﻿echo "Executing Start Up Script"
+﻿# echo "Executing Start Up Script"
 
 # the array of the databases to create
 dbs=(test_database audit elearning backup)
 
 for db in "${dbs[@]}"; do 
-    echo "Creating Database ${db}"
+    # echo "Creating Database ${db}"
     psql -U postgres -c "CREATE DATABASE ${db};"
     for file in sql/${db}/*.sql
     do
         # checks if a file exists, and if it does executes it
         if [ -f "$file" ]
         then
-            echo "Executing $file"
+            # echo "Executing $file"
             psql -d ${db} -f "$file"
         fi
     done
